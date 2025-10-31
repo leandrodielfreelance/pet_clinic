7c186fa035ea3cb15368b45fb4bea5d90b2d46a2
leodiel <leandro.jtl.tech@gmail.com>
2025-10-31 04:01:19 -0300

    test repo

--- src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
+++ src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
@@ -22,6 +22,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
+
 /**
  * Simple JavaBean domain object with an id property. Used as a base class for objects
  * needing this property.
